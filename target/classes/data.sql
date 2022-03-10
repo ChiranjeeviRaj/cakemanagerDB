068ed1886abcedf528d2dd01cff189b99e1b20d4
ChiranjeeviRaj <c.pinnamaraju@gmail.com>
2022-03-10 16:33:11 +0000

    Removed target

--- target/classes/data.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO CAKE(id, name, description, imageURL)
-values(10001, 'Lemon cheesecake', 'A cheesecake made of lemon' ,'https://s3-eu-west-1.amazonaws.com/s3.mediafileserver.co.uk/carnation/WebFiles/RecipeImages/lemoncheesecake_lg.jpg');
